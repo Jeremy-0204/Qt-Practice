745fb1cd0f87f85bc48ff4a53b5a2f61e47c792c
Jeremy Park <parkkyoungrun@gmail.com>
2023-06-15 17:22:10 +0900

    Update MGenSweepLaser.h
    - Q_PROPERTY 변수들에 대한 Getter 함수 선언

--- Test/MGenSweepLaser.h
+++ Test/MGenSweepLaser.h
@@ -6,7 +6,7 @@ MGEN_NAMESPACE_START
 class MGenSweepLaser : public QObject
 {
     Q_OBJECT
-    Q_PROPERTY(ELaserSweepMode mSweepMode READ mSweepMode WRITE setMSweepMode(ESuperlumLaserSweepMode SweepMode) NOTIFY mSweepModeChanged)
+    Q_PROPERTY(ESuperlumLaserSweepMode mSweepMode READ mSweepMode WRITE setMSweepMode(ESuperlumLaserSweepMode SweepMode) NOTIFY mSweepModeChanged)
     Q_PROPERTY(float mSingleToneWaveLength READ mSingleToneWaveLength WRITE setMSingleToneWaveLength(float SingleToneWaveLength) NOTIFY mSingleToneWaveLengthChanged)
     Q_PROPERTY(float mStartWaveLength READ mStartWaveLength WRITE setMStartWaveLength(float StartWaveLength) NOTIFY mStartWaveLengthChanged)
     Q_PROPERTY(float mStopWaveLength READ mStopWaveLength WRITE setMStopWaveLength(float StopToneWaveLength) NOTIFY mStopWaveLengthChanged)
@@ -26,13 +26,22 @@ public:
     virtual bool LaserOn() = 0;
     virtual bool LaserOff() = 0;
 
-
+    // Q_PROPERTY 변수들에 대한 Setter 함수
     virtual bool setMSweepMode(ESuperlumLaserSweepMode SweepMode) = 0;
     virtual bool setMSingleToneWaveLength(float SingleToneWaveLength) = 0;
     virtual bool setMStartWaveLength(float StartWaveLength) = 0;
     virtual bool setMStopWaveLength(float StopToneWaveLength) = 0;
     virtual bool setMSweepSpeed(float SweepSpeed) = 0;
     virtual bool setMPauseTime(float PauseTime) = 0;
+
+    // Q_PROPERTY 변수들에 대한 Getter 함수
+    ESuperlumLaserSweepMode mSweepMode();
+    float mSingleToneWaveLength();
+    float mStartWaveLength();
+    float mStopWaveLength();
+    float mSweepSpeed();
+    int mPauseTime();
+
 
 
 };
