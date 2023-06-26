70a250687dfa6a66ac164b9b0fe1a11985890df4
Jeremy Park <parkkyoungrun@gmail.com>
2023-06-26 14:25:51 +0900

    Feat. ViewModel on off 구현

--- Test/superlumbs840vm.cpp
+++ Test/superlumbs840vm.cpp
@@ -9,16 +9,35 @@ SuperlumBS840VM :: ~SuperlumBS840VM()
 
 bool SuperlumBS840VM :: onClickedConnect(const QString &portName, int baudRate, const QString &flowControl, const QString &parity, const QString &dataBits, const QString &stopBits)
 {
-    mSuperlumBS840.connectPort(portName, baudRate, flowControl, parity, dataBits, stopBits);
+    return mSuperlumBS840.connectPort(portName, baudRate, flowControl, parity, dataBits, stopBits);
 }
 
 bool SuperlumBS840VM :: onClickedClose()
 {
-    mSuperlumBS840.close();
+    return mSuperlumBS840.close();
 }
 
+//bool SuperlumBS840VM :: onClickedRefresh()
+//{}
 
+bool SuperlumBS840VM :: onClickedPowerOn()
+{
+    return mSuperlumBS840.powerOn();
+}
 
+bool SuperlumBS840VM :: onClickedPowerOff()
+{
+    return mSuperlumBS840.powerOff();
+}
 
+bool SuperlumBS840VM :: onClickedLaserOn()
+{
+    return mSuperlumBS840.laserOn();
+}
+
+bool SuperlumBS840VM :: onClickedLaserOff()
+{
+    return mSuperlumBS840.laserOff();
+}
 
 MGEN_NAMESPACE_END
